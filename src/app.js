db9c6283a3d2c65adc971952276a2afae363aa40
R-lite <muminraj15@gmail.com>
2023-02-03 17:59:19 +0100

    added lecturer, student server and designed schema for books

--- src/app.js
+++ src/app.js
@@ -11,8 +11,7 @@ const app = express();
 const { MONGO_URI } = process.env;
 // connect database
 
-mongoose
-  .connect(MONGO_URI, {
+mongoose.connect(MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
--- src/books/books.model.js
+++ src/books/books.model.js
@@ -2,7 +2,14 @@ import mongoose from "mongoose";
 
 export const BookSchema = new mongoose.Schema({
   title: String,
-  author: String,
+  tought_by: Array,
+  image_path: String,
+  stars: Number,
+  completion_time: String,
+  credit_load: Number,
+  number_of_registered: Number,
+  department: Array,
+  faculty: Array
 });
 
 export default mongoose.model("Book", BookSchema);
